2815ff9293847732c29d61bc89ca8472c665d6e8
Stephen Cowley <stephencowley44@gmail.com>
2021-08-12 13:25:58 -0700

    Added teclarc dotfile

--- linkdots.py
+++ linkdots.py
@@ -67,7 +67,7 @@ def link_dotfiles(config_path=None, files=None, include_git=False):
 
     filenames = ['vimrc', 'vim', 'bashrc', 'tmux.conf',
                  'inputrc', 'ipython/profile_default/ipython_config.py',
-                 'bash_profile']
+                 'bash_profile', 'teclarc']
 
     if include_git:
         filenames.append('gitconfig')
